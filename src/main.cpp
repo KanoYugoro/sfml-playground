20889e8c13e479d9ad93723eac3f8f165554803f
KanoYugoro <kanoyugoro@gmail.com>
2023-11-22 06:02:21 -0600

    Load and draw text

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,27 @@
 
 int main()
 {
+    sf::Font font;
+    if (!font.loadFromFile("./resources/fonts/KdamThmorPro-Regular.ttf"))
+    {
+        return 0;
+    }
+
+    sf::Text text;
+    text.setFont(font);
+    text.setString("This is a test of loading fonts in SFML.");
+    text.setCharacterSize(36);
+    text.setFillColor(sf::Color::White);
+    text.setPosition(100.f, 300.f);
+    
     sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
     sf::CircleShape shape;
     shape.setRadius(40.f);
     shape.setPosition(100.f, 100.f);
     shape.setFillColor(sf::Color::Cyan);
+
+
+
     while (window.isOpen())
     {
     sf::Event event;
@@ -17,6 +33,7 @@ int main()
     }
     window.clear();
     window.draw(shape);
+    window.draw(text);
     window.display();
     }
 }
