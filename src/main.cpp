183de98cc86a27815f73fe2b69c4a3e4b53df775
KanoYugoro <kanoyugoro@gmail.com>
2023-11-22 06:29:52 -0600

    Mockup RPG textbox

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,38 @@ int main()
     {
         return 0;
     }
+    const int GAME_WINDOW_WIDTH = 1200;
+    const int GAME_WINDOW_HEIGHT = 700;
+    const float TEXTBOX_X = 0;
+    const float TEXTBOX_Y = GAME_WINDOW_HEIGHT*0.75f;
+    const float TEXTBOX_BORDER_THICKNESS = 5;
+    const int FONT_SIZE = 16;
+    const int FONT_SHADOW_OFFSET = FONT_SIZE * 0.125f;
+
+    sf::Color grey(128,128,128,255);
+
+    sf::RenderWindow window(sf::VideoMode(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT), "Game");
+
+    sf::RectangleShape shape(sf::Vector2f(GAME_WINDOW_WIDTH-(TEXTBOX_BORDER_THICKNESS*2), (GAME_WINDOW_HEIGHT*.25f)-(TEXTBOX_BORDER_THICKNESS*2)));
+    shape.setPosition(TEXTBOX_X+TEXTBOX_BORDER_THICKNESS, TEXTBOX_Y+TEXTBOX_BORDER_THICKNESS);
+    shape.setFillColor(grey);
+    shape.setOutlineThickness(TEXTBOX_BORDER_THICKNESS);
+    shape.setOutlineColor(sf::Color::White);
 
     sf::Text text;
     text.setFont(font);
     text.setString("This is a test of loading fonts in SFML.");
-    text.setCharacterSize(36);
+    text.setCharacterSize(FONT_SIZE);
     text.setFillColor(sf::Color::White);
-    text.setPosition(100.f, 300.f);
+    text.setPosition(TEXTBOX_X+FONT_SIZE, TEXTBOX_Y+FONT_SIZE);
+
+    sf::Text text_shadow;
+    text_shadow.setFont(font);
+    text_shadow.setString("This is a test of loading fonts in SFML.");
+    text_shadow.setCharacterSize(FONT_SIZE);
+    text_shadow.setFillColor(sf::Color::Black);
+    text_shadow.setPosition(TEXTBOX_X+FONT_SIZE+FONT_SHADOW_OFFSET, TEXTBOX_Y+FONT_SIZE+FONT_SHADOW_OFFSET);
     
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
-    sf::CircleShape shape;
-    shape.setRadius(40.f);
-    shape.setPosition(100.f, 100.f);
-    shape.setFillColor(sf::Color::Cyan);
-
-
-
     while (window.isOpen())
     {
     sf::Event event;
@@ -33,6 +49,7 @@ int main()
     }
     window.clear();
     window.draw(shape);
+    window.draw(text_shadow);
     window.draw(text);
     window.display();
     }
